50e1ce68eff2909ebf356ee40a02eb31a08f79b9
Mitch's Guest <mitchsguest@MacBookPro.lan>
2025-02-04 23:11:25 -0500

    fix: add HTMLPreview component and correct import path

---
+++ src/components/HTMLPreview.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+  content: string;
+}
+
+const HTMLPreview = ({ content }: Props) => {
+  return (
+    <div
+      dangerouslySetInnerHTML={{ __html: content }}
+    />
+  );
+};
+
+export default HTMLPreview;
